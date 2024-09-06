1a700415af143dfee3f8354fc3fc01afcadb14cf
ingenarel <ingenarelitems@gmail.com>
2024-09-07 00:12:28 +0600

    ff

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ return {
 				"--no-status",
 				"--no-exit-code",
 				"--config",
-                "~/.config/yazi/plugins/omp.yazi/oh-my-posh-config.omp.json",
+                "C:\\Users\\Saad_Abdullah\\AppData\\Roaming\\yazi\\config\\plugins\\omp.yazi\\oh-my-posh-config.omp.json"
 			})
 			:cwd(args[1])
 			:output()
